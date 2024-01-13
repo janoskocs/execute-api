324dc0f9a0e6587dd7ec53274890bd6e21af576e
Janos Kocs <janos.kocs@outlook.com>
2024-01-13 23:55:41 +0000

    feat: implement basic sign up route, and fix migrations

--- app/controllers/signUp.controllers.js
+++ app/controllers/signUp.controllers.js
@@ -1,3 +1,4 @@
+const knex = require('knex')(require('../../knexfile'))
 const signUpUser = async (req, res) => {
   const user = req.body
   // Check if all data is provided
@@ -6,9 +7,23 @@ const signUpUser = async (req, res) => {
     return
   }
   // Check if email exists already in db
+  const userExists = await knex('user').where({ email: user.email }).first()
 
-  // Sign up the user
-  res.status(201).json({ hello: 'hello' })
+  if (userExists) {
+    res.status(400).json({ error: 'Email address already in use.' })
+    return
+  }
+
+  const signUp = await knex('user').insert({
+    first_name: user.first_name,
+    last_name: user.last_name,
+    handle: user.handle,
+    email: user.email,
+    password: user.password
+  })
+
+  const signedUpUser = await knex('user').where({ id: signUp[0] }).first()
+  res.status(201).json(signedUpUser)
 }
 
 module.exports = {
--- migrations/20240113200208_create_user_todo_tables.js
+++ migrations/20240113200208_create_user_todo_tables.js
@@ -11,8 +11,8 @@ exports.up = function (knex) {
       table.string('handle').notNullable()
       table.string('email').notNullable()
       table.string('password').notNullable()
-      table.integer('count_of_tasks_done').notNullable()
-      table.integer('number_of_followers').notNullable()
+      table.integer('count_of_tasks_done').notNullable().defaultTo(0)
+      table.integer('number_of_followers').notNullable().defaultTo(0)
       table.timestamp('created_at').defaultTo(knex.fn.now())
       table.timestamp('last_logged_in_time')
       table
