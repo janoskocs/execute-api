29e5f9c0efc536f71cb122390be9348c07362703
Janos Kocs <janos.kocs@outlook.com>
2024-01-14 15:18:19 +0000

    feature: implement authorization check middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('knex')(require('./knexfile'))
 const express = require('express')
 const app = express()
+const checkAuth = require('./app/middlewares/auth.middleware')
 const usersRoutes = require('./app/routes/users.routes')
 const todosRoutes = require('./app/routes/todos.routes')
 const loginRoutes = require('./app/routes/login.routes')
@@ -15,6 +16,7 @@ app.use('/api/v1/login', loginRoutes)
 
 app.use('/api/v1/signup', signUpRoutes)
 
+app.use(checkAuth)
 app.use('/api/v1/todos', todosRoutes)
 
 app.use('/api/v1/users', usersRoutes)
