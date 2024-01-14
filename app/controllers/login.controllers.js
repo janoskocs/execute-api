036678ef0d9c41576c77857a1341bdbbc2fd06bf
Janos Kocs <janos.kocs@outlook.com>
2024-01-14 14:09:23 +0000

    feature: implement login with encrypted passwords

--- app/controllers/login.controllers.js
+++ app/controllers/login.controllers.js
@@ -1,5 +1,23 @@
+const db = require('../models/user.model')
+const bcrypt = require('bcrypt')
 const loginUser = async (req, res) => {
-  res.send('Login user')
+  const userInput = req.body
+  // Check if all data is provided
+  if (!userInput.email || !userInput.password) {
+    res.status(400).json({ error: 'Invalid user info. Please check email, or password inputs.' })
+    return
+  }
+
+  const user = await db.getUserByEmail(userInput.email)
+
+  const comparePassword = await bcrypt.compare(userInput.password.toString(), user.password)
+
+  if (!comparePassword) {
+    res.status(401).send({ error: 'Invalid password.' })
+    return
+  }
+
+  res.status(200).send({ user: 'user logged in' })
 }
 
 module.exports = {
